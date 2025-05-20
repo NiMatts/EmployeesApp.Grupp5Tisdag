3e479448f9b20a7e4855e012b5452fcc7b1d7cd2
PathingStar <stanislav.hallberg@hallbergfaux.se>
2025-05-20 13:08:49 +0200

    email in index

--- EmployeesApp.Web/Views/Employees/CreateVM.cs
+++ EmployeesApp.Web/Views/Employees/CreateVM.cs
@@ -2,6 +2,10 @@
 {
     public class CreateVM
     {
+        public required string Name { get; set; }
 
+        public required string Email { get; set; }
+        
+        public required int BotCheck { get; set; }
     }
 }
